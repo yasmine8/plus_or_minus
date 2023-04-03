943fdb19a6221f5db89b007797c2ec5598ecc230
yasmine <yasmine159357@hotmail.fr>
2023-04-03 16:28:04 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    let mut strokes = 0;
+    //println!("the secret number is {secret_number}");
+    loop {
+        strokes +=1;
+        println!("please insert a number between 1 and 100:");
+        let mut supposition = String::new();
+        
+        io::stdin()
+            .read_line(&mut supposition)
+            .expect("Failed to read user input");
+    
+        let supposition: u32 = match supposition.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue
+        };
+        println!("Your number is: {supposition}");
+        
+        match supposition.cmp(&secret_number){
+            Ordering::Greater => println!("It's less !"),
+            Ordering::Less => println!("It's Greater !"),
+            Ordering::Equal => {
+                println!("You won with {strokes} strokes!");
+                break;
+            },
+        }
+    }      
+}
